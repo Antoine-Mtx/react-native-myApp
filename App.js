aa551821bc1e05405f5a345dc53e96d0074269ec
Antoine-Mtx <antoine.mortureux@gmail.com>
2023-02-21 11:32:40 +0100

    trying expo ImagePicker

--- App.js
+++ App.js
@@ -21,24 +21,33 @@
 //   );
 // }
 
-import React from "react";
-import { View, Text, Image, Button } from "react-native";
-import ImagePicker from "react-native-image-picker";
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default class App extends React.Component {
-  handleChoosePhoto = () => {
-    const options = {};
-    ImagePicker.launchImageLibrary(options, response => {
-      console.log('response', response);
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
   };
 
-  render() {
-    return (
-      // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View>
-        <Button title='Choisir votre photo' onPress={this.handleChoosePhoto}/>
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+  );
 }
